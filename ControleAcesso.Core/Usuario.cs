708435b8f045253b6e832df430fac591d62aa5a0
Isaac Oliveira Loiola <isaackopi@icloud.com>
2025-04-23 10:53:47 -0300

    Instanciando a propriedade Id no método Cadastrar

--- ControleAcesso.Core/Usuario.cs
+++ ControleAcesso.Core/Usuario.cs
@@ -36,6 +36,10 @@ namespace ControleAcesso.Core
             Senha = senha;
         }
 
+        /// <summary>
+        /// Método para inserir o usuario no banco de dados.
+        /// São necessarios as seguintes propriedades: Nome, Cpf, TipoUsuario e Senha
+        /// </summary>
         public void Cadastrar()
         {
             var cmd = Banco.Abrir();
@@ -43,6 +47,8 @@ namespace ControleAcesso.Core
             cmd.CommandText = $"insert into usuarios(nome, cpf, tipousuario, senha) " +
                               $"values ('{Nome}', '{Cpf}', {TipoUsuario}, '{Senha}')";
             cmd.ExecuteReader();
+            cmd.CommandText = "select last_insert_id()";
+            Id = Convert.ToInt32(cmd.ExecuteScalar());
         }
 
     }
